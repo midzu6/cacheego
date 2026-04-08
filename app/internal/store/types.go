8a25c9a9f323e1ab9cffa88860cef2454ec8862b
midzuズTokyo <yotsume52@gmail.com>
2026-04-08 21:38:26 +0300

    fix(types): исправить поле структуры для доступа из другого пакета

--- app/internal/store/types.go
+++ app/internal/store/types.go
@@ -1,24 +1,29 @@
 package store
 
 type RedisValue interface {
-	Name() string
+	Type() string
 	String() string
 	Size() int64
+	Bytes() []byte
 }
 
 // StringValue value in store map
 type StringValue struct {
-	data []byte
+	Data []byte
 }
 
-func (sv *StringValue) Name() string {
+func (sv StringValue) Type() string {
 	return "string"
 }
 
-func (sv *StringValue) String() string {
-	return string(sv.data)
+func (sv StringValue) String() string {
+	return string(sv.Data)
 }
 
-func (sv *StringValue) Size() int64 {
-	return int64(len(sv.data))
+func (sv StringValue) Size() int64 {
+	return int64(len(sv.Data))
+}
+
+func (sv StringValue) Bytes() []byte {
+	return sv.Data
 }
